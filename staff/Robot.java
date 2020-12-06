5f8cc1f635e994eae15b9c5207fbdcc545c44905
Andreas Koehler <and.koehler.19@lehre.mosbach.dhbw.de>
2020-12-06 18:07:24 +0100

    Programm

--- staff/Robot.java
+++ staff/Robot.java
@@ -1,8 +1,8 @@
 package staff;
 
+import baggageScanner.Piece;
 import passenger.HandBaggage;
 import passenger.Layer;
-import baggageScanner.Piece;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -13,8 +13,8 @@ public class Robot {
         Piece[] pieces = new Piece[1000];
         String content = Arrays.stream(baggage.getLayer()).map(Layer::getContent).collect(Collectors.joining(""));
 
-        for(int i=0; i<1000; i++){
-            pieces[i] = new Piece(content.substring(i*50, (i+1)*50));
+        for (int i = 0; i < 1000; i++) {
+            pieces[i] = new Piece(content.substring(i * 50, (i + 1) * 50));
         }
 
         return pieces;
